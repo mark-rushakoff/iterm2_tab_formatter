3c98b1bd174bc30b45d701d98ecd603bfa373c50
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-03-17 22:44:02 -0700

    Attempt workaround for root class inheriting from other library

--- lib/iterm2_tab_formatter/version.rb
+++ lib/iterm2_tab_formatter/version.rb
@@ -1,4 +1,8 @@
+begin
 require 'iterm2_tab_formatter/base'
+rescue LoadError
+  warn "Couldn't load base class for Iterm2TabFormatter.  This might be okay if reading from the gemspec before running bundle install."
+end
 
 class Iterm2TabFormatter
   VERSION = "0.0.1"

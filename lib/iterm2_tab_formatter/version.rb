e7c95165e501b2f51826274c1c90197c8f3c463f
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-03-18 23:10:12 -0700

    Loosen RSpec requirement, bump to 0.7.1

--- lib/iterm2_tab_formatter/version.rb
+++ lib/iterm2_tab_formatter/version.rb
@@ -1,9 +1,9 @@
 begin
-require 'iterm2_tab_formatter/base'
+  require 'iterm2_tab_formatter/base'
 rescue LoadError
   warn "Couldn't load base class for Iterm2TabFormatter.  This might be okay if reading from the gemspec before running bundle install."
 end
 
 class Iterm2TabFormatter
-  VERSION = "0.7.0"
+  VERSION = "0.7.1"
 end
